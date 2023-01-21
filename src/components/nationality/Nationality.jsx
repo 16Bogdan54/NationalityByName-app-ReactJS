f8b17dca656e7fd3856b659c53cb34e77a6bbfbf
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-01-21 17:39:17 +0200

    zero check

--- src/components/nationality/Nationality.jsx
+++ src/components/nationality/Nationality.jsx
@@ -5,20 +5,22 @@ export default function Nationality({ data }) {
     <div>
       <p>Max probability is 1</p>
       {Object.values(data).map((item, index) => {
-        return (
-          <p key={index} style={{ margin: ".5rem 0rem" }}>
-            {item.country_id}
-            <span>
+          if(item.probability > 0) {
+              return (
+                  <p key={index} style={{ margin: ".5rem 0rem" }}>
+                      {item.country_id}
+                      <span>
               <img
-                alt={item.country_id}
-                width="30px"
-                height="15px"
-                src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${item.country_id}.svg`}
+                  alt={item.country_id}
+                  width="30px"
+                  height="15px"
+                  src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${item.country_id}.svg`}
               />
             </span>
-            <span>{Math.floor(item.probability * 100) / 100}</span>
-          </p>
-        );
+                      <span>{Math.floor(item.probability * 100) / 100}</span>
+                  </p>
+              );
+          }
       })}
     </div>
   );
